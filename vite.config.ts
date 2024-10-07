3724c6017a5c9c4e2fe8183296117acc7b69f7fd
Simon Steinhauser <steinhauser.simon@gmail.com>
2024-10-07 21:41:01 +0200

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ injectRegister: "auto" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
