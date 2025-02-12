c60e60f621c928c642c1e873dda79d578fc8acca
Simon <steinhauser.simon@gmail.com>
2025-02-12 12:11:35 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/satisfactory-production-optimizer/sw.js', { scope: '/satisfactory-production-optimizer/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
